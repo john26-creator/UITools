7f0442a45d425d4b6fc494191a66f3285c37ccc9
Jonathan <jonathan.amselem.pro@gmail.com>
2021-07-21 15:19:28 +0200

    amélioration du formualaire

--- contactForm/script.php
+++ contactForm/script.php
@@ -10,34 +10,27 @@ $message = $_POST["message"];
 
 $status = true; //statut du formaulaire et de l'envoie du mail
 
-function isInformed ($field) { //Est ce qu'un champ est renseigné
+$mailRegexp = "/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}$/";
+
+if (!isInformed($message) || !isInformed($nom) ||
+    !isInformed($email)   || !preg_match($mailRegexp, $email)) {
+    $status = false;
+}
+
+function isInformed($field)
+{ //Est ce qu'un champ est renseigné
     return isset($field) && !empty($field);
-}
-
-if (isInformed($email)) {  // Est ce que l'email est renseigné ?
-    $mailRegexp = "/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}$/";
-    if(!preg_match($mailRegexp, $email))  { // Est ce qu'il a un format de mail ?
-        $status = false;
-    } 
-} else {
-    $status = false;
-}
-
-if (!isInformed($message)) {
-    $status =  false;
-}
-if (!isInformed($nom)) {
-    $status = false;
 }
 session_start();
 
 if ($status) {
     $headers = "From: $email" . "\r\n" .
-     "Reply-To: $email" . "\r\n" .
-     'X-Mailer: PHP/' . phpversion();
+        "Reply-To: $email" . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
     $status = mail("jonathan.amselem.pro@gmail.com", $sujet, $message, $headers);
-} if ($status) {
-    setcookie("nom", $nom, time()+3600);
+}
+if ($status) {
+    setcookie("nom", $nom, time() + 3600);
     setcookie("prenom", $prenom);
     setcookie("message", $message);
     setcookie("sujet", $sujet);
@@ -45,10 +38,8 @@ if ($status) {
     setcookie("confmail", $confEmail);
 
     $_SESSION["session"] = "bonjour $nom, $prenom";
-}   
- else {
+} else {
     session_destroy();
 }
 
-//var_dump($email);
 echo json_encode(['status' => $status]);
